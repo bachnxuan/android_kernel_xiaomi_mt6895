a0c09e821b134d4b9ae74723ce3abe2e40a5ee7d
Bachpooh <76820483+bachnxuan@users.noreply.github.com>
2025-02-07 11:47:56 +0700

    build.sh: Update LOCALVERSION

--- build.sh
+++ build.sh
@@ -27,7 +27,7 @@ DATE_START=$(date +"%s")
 
 make $THREAD CC="ccache clang" CXX="ccache clang++" $LLVM_CONFIG $DEFCONFIG O=out
 make $THREAD CC="ccache clang" CXX="ccache clang++" $LLVM_CONFIG \
-    CONFIG_LOCALVERSION_AUTO=n LOCALVERSION=-android12-9 \
+    CONFIG_LOCALVERSION_AUTO=n LOCALVERSION=-android12-gki \
     LTO=thin O=out 2>&1 | tee kernel.log
 
 DATE_END=$(date +"%s")
